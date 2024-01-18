bd29a42da49c9aab8d376db6aafcb40a59d2cc1a
AnonymRo <147411147+AnonymRo@users.noreply.github.com>
2024-01-18 14:45:10 +0200

    Updates

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-// Menu animation
 document.addEventListener("DOMContentLoaded", function () {
     var menuIcon = document.getElementById("menu");
     var overlay = document.getElementById("overlay");
     var nav = document.querySelector("nav");
-    const menuIconDelay = 1000;
-
+  
     if (menuIcon) {
-        menuIcon.onclick = function () {
-            menuIcon.classList.toggle("openmenu");
-            overlay.classList.toggle("active");
-            nav.classList.toggle("open");
-        }
-    }  
-});
+      menuIcon.onclick = function () {
+        menuIcon.classList.toggle("openmenu");
+        overlay.classList.toggle("active");
+        nav.classList.toggle("open");
+      }
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const words = document.querySelectorAll('.word');
+  
+    words.forEach((word, wordIndex) => {
+      const letters = word.textContent.split('');
+      word.innerHTML = '';
+  
+      letters.forEach((letter, index) => {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        word.appendChild(span);
+  
+        // Adjust the delay based on your preference
+        const delay = wordIndex * 300 + index * 100;
+  
+        setTimeout(() => {
+          span.style.opacity = '1';
+        }, delay);
+      });
+    });
+  });
+  
+  
+  
+  
