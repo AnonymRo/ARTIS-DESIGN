a93336869f2f09cd878832ce54593cde3620a168
AnonymRo <147411147+AnonymRo@users.noreply.github.com>
2024-01-23 16:47:23 +0200

    .

--- script.js
+++ script.js
@@ -51,4 +51,16 @@ document.addEventListener("DOMContentLoaded", function () {
       }, delay);
     });
   });
+
+  /* Back to the top button */
+
+  const toTop = document.querySelector(".to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      toTop.classList.add("active");
+    } else {
+      toTop.classList.remove("active");
+    }
+  })
 });
